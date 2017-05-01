df4e62a0f529da61de836682b5488143c94e1957
nano2nd <javant@iihs.org>
2017-04-30 21:59:05 -0400

    Updating IP for publish

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync -a -f"+ */" -f"+ *.py" -f"- *" $(pwd)/ osmc@192.168.1.4:/home/osmc/Developer/vz_bill_watcher/
+rsync -a -f"+ */" -f"+ *.py" -f"- *" $(pwd)/ osmc@192.168.1.5:/home/osmc/Developer/vz_bill_watcher/
